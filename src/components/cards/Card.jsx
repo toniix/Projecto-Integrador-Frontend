dc472ca4ccb300e91e7e402109e2cc3d6fa4a4d4
ClaReMa7 <cla.rm377@gmail.com>
2025-03-20 00:07:43 -0500

    fix: se soliciona llamada del precio en lista de favoritos

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,4 +1,3 @@
-
 import PropTypes from "prop-types";
 import Button from "../common/Button";
 import FavoriteButton from "../common/FavoriteButton";
@@ -6,28 +5,38 @@ import FavoriteButton from "../common/FavoriteButton";
 const Card = ({ product, onViewDetail }) => {
   return (
     <div
-      className="relative cursor-pointer bg-white shadow-2xl rounded-3xl"
+      className="relative cursor-pointer bg-white shadow-2xl rounded-3xl flex flex-col h-full"
       onClick={() => onViewDetail(product.idProduct)}
     >
       {/* Botón de favorito */}
       <FavoriteButton productId={product.idProduct} />
-
+      
       <img
-        src={`/provisorio/${product.idProduct}.jpg`} alt={product.name}
+        src={`/provisorio/${product.idProduct}.jpg`} 
+        alt={product.name}
         className="w-full h-60 object-cover rounded-t-2xl"
+        onError={(e) => {
+          e.target.src = "/img/placeholder.jpg";
+        }}
       />
-      <div className="p-6 flex flex-col flex-grow">
-        <h3 className="text-2xl text-[#1E1E1E]">{product.name}</h3>
-        <div className="flex justify-between items-center mt-4">
+      <div className="p-6 flex flex-col flex-1">
+        <h3 className="text-2xl text-[#1E1E1E] mb-2">{product.name}</h3>
+        {/* Este div empuja el contenido que sigue hacia abajo */}
+        <div className="flex-1"></div>
+        <div className="flex justify-between items-center mt-auto">
           <h2 className="text-2xl text-[#C78418]">{`$${product.price}`}</h2>
-          <Button onClick={() => onViewDetail(product.idProduct)}>Ver</Button>
+          <Button onClick={(e) => {
+            e.stopPropagation(); // Evita que se propague al div padre
+            onViewDetail(product.idProduct);
+          }}>
+            Ver
+          </Button>
         </div>
-
-
       </div>
     </div>
   );
 };
+
 Card.propTypes = {
   product: PropTypes.shape({
     idProduct: PropTypes.number.isRequired,
